4f30cdd7a326e41d300469a1322a6743abcc7d86
Ivan Tsar <ivan.tsar99@gmail.com>
2018-02-25 16:50:20 +0300

    3rd solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,66 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.len = 0;
   }
 
   add(element) {
-    // your implementation
+    this.len =this.len + 1;
+    this.array[this.len-1]=element;
   }
 
   at(index) {
-    // your implementation
+   return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.len;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices = []) {
+    let temp = [], ind = [];
+
+    for (let i = 0; i < indices.length; i++) {
+      ind[i] = indices[i];
+    }
+
+    for (let i = 0; i < ind.length; i++) {
+      for (let j = i + 1; j < ind.length; j++) {
+        if (ind[i] > ind[j]) {
+          let boof = ind[i];
+          ind[i] = ind[j];
+          ind[j] = boof;
+          i = 0;
+        }
+      }
+    }
+
+    for (let i = 0; i < ind.length; i++) {
+      temp[i] = this.array[ind[i]];
+    }
+
+    //bubble sort
+    for (let i = 0; i < temp.length; i++) {
+      for (let j = i + 1; j < temp.length; j++) {
+        if (temp[i] > temp[j]) {
+          let boof = temp[i];
+          temp[i] = temp[j];
+          temp[j] = boof;
+          i = 0;
+        }
+      }
+    }
+
+    for (let i = 0; i < ind.length; i++)
+      this.array[ind[i]] = temp[i];
   }
 
   setComparator(compareFunction) {
-    // your implementation
+  
   }
 }
 
